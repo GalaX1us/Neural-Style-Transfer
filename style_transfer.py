b3c8ff6c72632983522a524c42d58ca788739a0b
GalaX1us <75265945+GalaX1us@users.noreply.github.com>
2023-01-27 22:55:44 +0100

    new algo to use LBFGS

--- style_transfer.py
+++ style_transfer.py
@@ -1,45 +1,92 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import absolute_import
+
+import numpy as np
+from PIL import Image
 from utils import *
-import time
+from params import *
 from model import *
+import time
+from LBFGS import LBFGSOptimizer
 
-content_image = load_img(CONTENT_PATH)
-style_image = load_img(STYLE_PATH)
+extractor = StyleContentModel(style_layers=STYLE_LAYERS, content_layers=CONTENT_LAYERS)
 
-vgg = tf.keras.applications.VGG19(include_top=False, weights='imagenet', pooling='max')
+base_image = load_and_process_img(CONTENT_PATH)
 
+style_targets_list, content_target = get_feature_representations(extractor, CONTENT_PATH, STYLE_PATHS)
 
-extractor = StyleContentModel(style_layers, content_layers)
+x = tf.Variable(base_image, trainable=True)
 
-style_targets = extractor(style_image)['style']
-content_targets = extractor(content_image)['content']
+class InputWrapper(tf.keras.Model):
+    def __init__(self, x: tf.Variable):
+        super(InputWrapper, self).__init__()
 
-@tf.function()
-def train_step(image):
-  with tf.GradientTape() as tape:
-    outputs = extractor(image)
-    loss = style_content_loss(outputs, style_targets, content_targets)
-    loss += TOTAL_VARIATION_WEIGHT*tf.image.total_variation(image)
+        self.x = x
 
-  grad = tape.gradient(loss, image)
-  opt.apply_gradients([(grad, image)])
-  image.assign(clip_0_1(image))
+        self.vgg = extractor
+        self.vgg.trainable = False
 
-opt = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE,beta_1 = BETA_1,beta_2 = BETA_2, epsilon=EPSILON)
-image = tf.Variable(content_image)
+    def call(self, inputs, training=None):
+        outputs = self.vgg(self.x)
+        return outputs
 
-start = time.time()
+x_wrapper = InputWrapper(x)
 
-step = 0
-for n in range(EPOCHS):
-  for m in range(STEPS_PER_EPOCHS):
-    step += 1
-    train_step(image)
-    print(".", end='', flush=True)
-  print("Train step: {}".format(step))
+@tf.function  # (tracing will be done by LBFGS for us)
+def loss_wrapper(model):
+    outputs = model(x)
+    content_losses, style_losses, tv_losses = compute_loss(x, outputs, content_target, style_targets_list)
+    loss = content_losses + style_losses + tv_losses
+    return loss
 
-end = time.time()
-print("Total time: {:.1f}".format(end-start))
-tensor_to_image(image).save(getOutputFileName())
+prev_min_val = -1
+start_time = time.time()
 
 
+def save_image_callback(model, info_dict=None):
+    """
+    Callback function to get infos while training
+    """
+    global prev_min_val, start_time
 
+    info_dict = info_dict or {}
+    loss_value = info_dict.get('loss', None)
+    i = info_dict.get('iter', -1)
+
+    if loss_value is not None:
+        loss_val = loss_value.numpy()
+
+        if prev_min_val == -1:
+            prev_min_val = loss_val
+
+        improvement = (prev_min_val - loss_val) / prev_min_val * 100
+
+        print("Current loss value:", loss_val, " Improvement : %0.3f" % improvement, "%")
+        prev_min_val = loss_val
+
+    last_save = info_dict.get('last_save', False)
+    if (i + 1) % 100 == 0 or last_save:
+        img = model.x.numpy()
+        # save current generated image
+        img = deprocess_img(img)
+
+        if not last_save:
+            fname = OUTPUT_PATH + "out" + "_at_iteration_%d.png" % (i + 1)
+        else:
+            fname = OUTPUT_PATH + "out" + "_final.png"
+
+        imsave(fname, img)
+        end_time = time.time()
+        print("Image saved as", fname)
+        print("Iteration %d completed in %ds" % (i + 1, end_time - start_time))
+
+
+optimizer = LBFGSOptimizer(max_iterations=EPOCHS, tolerance=TOLERANCE)
+
+# Add a save image callback to this
+optimizer.register_callback(save_image_callback)
+
+optimizer.minimize(loss_wrapper, x_wrapper)
+
+save_image_callback(x_wrapper, info_dict={'last_save': True})
